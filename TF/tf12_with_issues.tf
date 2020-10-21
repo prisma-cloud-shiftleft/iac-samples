fae1fab083848a03ce3dd6bb5c6fc174710eb477
Giang Nguyen <ginguyen@paloaltonetworks.com>
2020-10-20 17:52:58 -0700

    fixed "log_bucket" name

--- TF/tf12_with_issues.tf
+++ TF/tf12_with_issues.tf
@@ -6,7 +6,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "log_bucket" {
+resource "aws_s3_bucket" "foo" {
   bucket = "my-tf-log-bucket"
   acl = "public-read-write"
 }
